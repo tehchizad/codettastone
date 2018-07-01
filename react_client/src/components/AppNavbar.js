f8ae0e4e9765f9ed8ff4902d9576c9f388681b0f
The Chad <tehchizad@gmail.com>
2018-07-01 10:51:21 -0600

    working react_app and backend

--- react_client/src/components/AppNavbar.js
+++ react_client/src/components/AppNavbar.js
@@ -1,6 +1,4 @@
-import React, {
-  Component
-} from 'react'
+import React, { Component } from 'react';
 import {
   Collapse,
   Navbar,
@@ -10,39 +8,38 @@ import {
   NavItem,
   NavLink,
   Container
-} from 'reactstrap'
+} from 'reactstrap';
 
 class AppNavbar extends Component {
   state = {
     isOpen: false
-  }
- 
+  };
+
   toggle = () => {
     this.setState({
       isOpen: !this.state.isOpen
-    })
-  }
+    });
+  };
 
   render() {
-    return(
+    return (
       <div>
-      <Navbar color="dark" dark expand="sm" className="mb-5">
-        <Container>
-          <NavbarBrand href="/">Shopping List</NavbarBrand>
-          <NavbarToggler onClick={this.toggle}/>
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="https://github.com/tehchizad">Github</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Container> 
-      </Navbar>
-    </div>
-    )
+        <Navbar color="dark" dark expand="sm" className="mb-5">
+          <Container>
+            <NavbarBrand href="/">Shopping List</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="https://github.com/tehchizad">Github</NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
   }
 }
 
-
-export default AppNavbar
+export default AppNavbar;
