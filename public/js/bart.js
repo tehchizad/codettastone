b347b85c7d9bb0924917fd2c37de315579a193f7
Chad Infanger <infanger@infanger-macbookpro.roam.corp.google.com>
2019-02-12 16:55:30 -0800

    bart map (broken)

--- public/js/bart.js
+++ public/js/bart.js
@@ -1,4 +1,5 @@
 const textbox = document.getElementById("output"),
+  topbox = document.getElementById("header"),
   bartKey = "MW9S-E7SL-26DU-VV8V",
   mapsKey = "AIzaSyBCFO3Lg7DaNsVjeBpK3CBalUEH3RfEANE",
   walkTime = 12,
@@ -6,44 +7,65 @@ const textbox = document.getElementById("output"),
   abbr = "embr"
 
 let output = ""
+let header = ""
 
 function getAPI() {
-  let bartURI = `http://api.bart.gov/api/etd.aspx?key=${bartKey}&cmd=etd&orig=${abbr}&json=y`
+  let bartURI = `https://api.bart.gov/api/etd.aspx?key=${bartKey}&cmd=etd&orig=${abbr}&json=y`
 
   fetch(bartURI)
     .then(response => response.json())
     .then(data => {
       let message = data.root.station[0],
         departTimes = message.etd
-      output += `<h1 class="h3 mb-3 font-weight-normal">bart departures: ${message.name} station</h1>`
+      header += `<h1 class="h3 mb-3 font-weight-normal">bart departures: ${
+        message.name
+      } station</h1>`
       departTimes.forEach(city => {
         parseArrivalTimes(city)
       })
       textbox.innerHTML = output
+      topbox.innerHTML = header
       console.log(now)
     })
     .catch(error => console.log(error))
 }
 
 function parseArrivalTimes(city) {
+  console.log("city stuff", city.estimate[0])
+  output += `
+  <hr>
+        <ul class="list-inline">
+        <li class="list-inline-item">
+        ${city.destination} ${city.estimate[0].direction} bound
+        </li>
+        <hr color="${city.estimate[0].hexcolor}">`
   city.estimate.forEach(time => {
     let etd = time.minutes
-    if (etd != "Leaving" && etd >= walkTime) {
+    if (etd == "Leaving") {
+      output += `
+      <ul class="list-inline">
+        <li class="list-inline-item">
+          ${etd}
+        </li>    
+      </ul>`
+    } else if (etd != "Leaving" && etd >= walkTime) {
       let departure = new Date(now.getTime() + etd * 60000 + time.delay * 60000),
         adjustedDeptarture = new Date(departure - walkTime * 60000)
       output += `
       <ul class="list-inline">
         <li class="list-inline-item">
-        ${city.destination} ${time.direction}bound - ${etd} minutes
+          ${etd} minutes
         </li>
         <li class="list-inline-item">
         leave by ${adjustedDeptarture.toLocaleTimeString()}
-        </li>
-        <div class="progress" style="height:5px">
-          <div class="progress-bar" role="progressbar" style="width: 100%;background-color:${
-            time.hexcolor
-          }"/>
-        </div>
+        </li>        
+      </ul>`
+    } else {
+      output += `
+      <ul class="list-inline">
+        <li class="list-inline-item">
+          ${etd} minutes
+        </li>    
       </ul>`
     }
   })
---
+++ public/js/maps.js
@@ -0,0 +1,241 @@
+const options = {
+    zoom: 13,
+    center: { lat: 37.773972, lng: -122.431297 }
+  },
+  markers = [
+    {
+      position: { lat: 37.7929, lng: -122.3971 }
+    }
+  ]
+
+function initMap() {
+  let map = new google.maps.Map(document.getElementById("map"), options),
+    styledMapType = new google.maps.StyledMapType(styleObj, { name: "Styled Map" })
+
+  map.mapTypes.set("styled_map", styledMapType)
+  map.setMapTypeId("styled_map")
+  initMarkers(map)
+}
+
+function initMarkers(map) {
+  markers.forEach(mark => {
+    mark.map = map
+    let marker = new google.maps.Marker(mark)
+  })
+}
+
+const styleObj = [
+  {
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#ebe3cd"
+      }
+    ]
+  },
+  {
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#523735"
+      }
+    ]
+  },
+  {
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#f5f1e6"
+      }
+    ]
+  },
+  {
+    featureType: "administrative",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#c9b2a6"
+      }
+    ]
+  },
+  {
+    featureType: "administrative.land_parcel",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#dcd2be"
+      }
+    ]
+  },
+  {
+    featureType: "administrative.land_parcel",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#ae9e90"
+      }
+    ]
+  },
+  {
+    featureType: "landscape.natural",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#dfd2ae"
+      }
+    ]
+  },
+  {
+    featureType: "poi",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#dfd2ae"
+      }
+    ]
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#93817c"
+      }
+    ]
+  },
+  {
+    featureType: "poi.park",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#a5b076"
+      }
+    ]
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#447530"
+      }
+    ]
+  },
+  {
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#f5f1e6"
+      }
+    ]
+  },
+  {
+    featureType: "road.arterial",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#fdfcf8"
+      }
+    ]
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#f8c967"
+      }
+    ]
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#e9bc62"
+      }
+    ]
+  },
+  {
+    featureType: "road.highway.controlled_access",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#e98d58"
+      }
+    ]
+  },
+  {
+    featureType: "road.highway.controlled_access",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#db8555"
+      }
+    ]
+  },
+  {
+    featureType: "road.local",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#806b63"
+      }
+    ]
+  },
+  {
+    featureType: "transit.line",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#dfd2ae"
+      }
+    ]
+  },
+  {
+    featureType: "transit.line",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#8f7d77"
+      }
+    ]
+  },
+  {
+    featureType: "transit.line",
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#ebe3cd"
+      }
+    ]
+  },
+  {
+    featureType: "transit.station",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#dfd2ae"
+      }
+    ]
+  },
+  {
+    featureType: "water",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#b9d3c2"
+      }
+    ]
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#92998d"
+      }
+    ]
+  }
+]
