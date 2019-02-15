a1c03dac74e871a934ee44783d05472c9dbd2828
Chad Infanger <infanger@infanger-macbookpro.roam.corp.google.com>
2019-02-14 18:08:44 -0800

    refactor & delay correction

--- public/js/bart.js
+++ public/js/bart.js
@@ -1,71 +1,91 @@
 const textbox = document.getElementById("output"),
   topbox = document.getElementById("header"),
-  bartKey = "MW9S-E7SL-26DU-VV8V",
-  mapsKey = "AIzaSyBCFO3Lg7DaNsVjeBpK3CBalUEH3RfEANE",
-  walkTime = 12,
-  now = new Date(),
-  abbr = "embr"
+  mapsKey = "AIzaSyBCFO3Lg7DaNsVjeBpK3CBalUEH3RfEANE"
 
-let output = ""
-let header = ""
+let output = "",
+  header = "",
+  walkTime,
+  message = "",
+  directionsService,
+  directionsDisplay
+
+function initMap() {
+  styleObj = fetch("json/mapstyle.json")
+    .then(response => response.json())
+    .then(styleObj => {
+      let map = new google.maps.Map(document.getElementById("map"), options),
+        styledMapType = new google.maps.StyledMapType(styleObj, { name: "Styled Map" })
+
+      directionsService = new google.maps.DirectionsService()
+      directionsDisplay = new google.maps.DirectionsRenderer()
+
+      map.mapTypes.set("styled_map", styledMapType)
+      map.setMapTypeId("styled_map")
+      map.setCenter(gMarker)
+      directionsDisplay.setMap(map)
+      initMarkers(map)
+    })
+}
 
 function getAPI() {
-  let bartURI = `https://api.bart.gov/api/etd.aspx?key=${bartKey}&cmd=etd&orig=${abbr}&json=y`
+  let bartKey = "MW9S-E7SL-26DU-VV8V",
+    abbr = "embr",
+    bartURI = `https://api.bart.gov/api/etd.aspx?key=${bartKey}&cmd=etd&orig=${abbr}&json=y`
 
   fetch(bartURI)
     .then(response => response.json())
     .then(data => {
-      let message = data.root.station[0],
-        departTimes = message.etd
-      header += `<h3 class="cover-heading">
-      ${message.name} station</h3>`
-      departTimes.forEach(city => {
-        parseArrivalTimes(city)
-      })
-      textbox.innerHTML = output
-      topbox.innerHTML = header
+      message = data.root.station[0]
+      populateOutput()
     })
     .catch(error => console.log(error))
 }
 
-function parseArrivalTimes(city) {
-  output += `
-  <ul class="list-inline">
-  <li class="list-inline-item border rounded-pill" style="padding:2px 10px;border-color:${
-    city.estimate[0].hexcolor
-  }!important">
-  ${city.destination} ${city.estimate[0].direction} bound
-  </li>
-  `
-  city.estimate.forEach(time => {
-    let etd = time.minutes
-    if (etd == "Leaving") {
-      output += `
-      <ul class="list-inline">
-        <li class="list-inline-item">
-          ${etd}
-        </li>    
-      </ul>`
-    } else if (etd != "Leaving" && etd >= walkTime) {
-      let departure = new Date(now.getTime() + etd * 60000 + time.delay * 60000),
-        adjustedDeptarture = new Date(departure - walkTime * 60000)
-      output += `
-      <ul class="list-inline">
-        <li class="list-inline-item">
-          ${etd} minutes
-        </li>
-        <li class="list-inline-item">leave by ${adjustedDeptarture.toLocaleTimeString()}
-        </li>        
-      </ul>`
-    } else {
-      output += `
-      <ul class="list-inline">
-        <li class="list-inline-item">
-          ${etd} minutes
-        </li>    
-      </ul>`
+function populateOutput() {
+  header += `<h3 class="cover-heading">${message.name} station</h3>`
+  message.etd.forEach(city => {
+    calcRoute(directionsService, directionsDisplay, city)
+  })
+  topbox.innerHTML = header
+}
+
+function calcRoute(directionsService, directionsDisplay, city) {
+  var start = gMarker
+  var end = embarcaderoMarker
+  var request = {
+    origin: start,
+    destination: end,
+    travelMode: "WALKING"
+  }
+  directionsService.route(request, (result, status) => {
+    if (status == "OK") {
+      walkTime = result.routes[0].legs[0].duration.value / 60
+      directionsDisplay.setDirections(result)
+      parseArrivalTimes(city)
     }
   })
+}
+
+function parseArrivalTimes(city) {
+  output += `<ul class="list-inline"><li class="list-inline-item border rounded-pill" style="padding:2px 10px;border-color:
+  ${city.estimate[0].hexcolor}!important">
+  ${city.destination} ${city.estimate[0].direction} bound</li>`
+  city.estimate.forEach(time => {
+    output += `<ul class="list-inline"><li class="list-inline-item">`
+    let etd = time.minutes
+    if (etd != "Leaving" && etd >= walkTime) {
+      let now = new Date(),
+        departure = new Date(now.getTime() + etd * 60000 + time.delay * 60),
+        adjustedDeptarture = new Date(departure - walkTime * 60000)
+      output += `${etd} minutes</li><li class="list-inline-item">leave by ${adjustedDeptarture.toLocaleTimeString()}</li></ul>`
+      console.log("depart",time.delay)
+    } else if (etd == "Leaving") {
+      output += `${etd}</li></ul>`
+    } else {
+      output += `${etd} minutes</li></ul>`
+    }
+  })
+  textbox.innerHTML = output
 }
 
 getAPI()
--- public/js/maps.js
+++ public/js/maps.js
@@ -1,55 +1,25 @@
-const options = {
-    zoom: 13,
-    center: { lat: 37.789739, lng: -122.389832 }
+const gMarker = { lat: 37.78954, lng: -122.389988 },
+  options = {
+    zoom: 14,
+    disableDefaultUI: true,
+    center: gMarker
   },
+  embarcaderoMarker = { lat: 37.7929, lng: -122.3971 },
+  missionMarker = { lat: 37.7844, lng: -122.395 },
+  bartImg = "https://maps.gstatic.com/mapfiles/transit/iw2/6/us-ca-bart.png",
   markers = [
     {
-      position: { lat: 37.7929, lng: -122.3971 }
+      position: embarcaderoMarker,
+      title: "Bart",
+      icon: bartImg
+    },
+    {
+      position: missionMarker,
+      title: "Bart",
+      icon: bartImg
     }
   ]
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos)
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation."
-  )
-  infoWindow.open(map)
-}
-
-function initMap() {
-  let map = new google.maps.Map(document.getElementById("map"), options),
-    styledMapType = new google.maps.StyledMapType(styleObj, { name: "Styled Map" }),
-    infoWindow = new google.maps.InfoWindow()
-
-  map.mapTypes.set("styled_map", styledMapType)
-  map.setMapTypeId("styled_map")
-  // Try HTML5 geolocation.
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        let pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        }
-
-        infoWindow.setPosition(pos)
-        infoWindow.setContent("Location found.")
-        infoWindow.open(map)
-        map.setCenter(pos)
-        console.log("pos",pos)
-      },
-      function() {
-        handleLocationError(true, infoWindow, map.getCenter())
-      }
-    )
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter())
-  }
-  initMarkers(map)
-}
 
 function initMarkers(map) {
   markers.forEach(mark => {
@@ -57,219 +27,3 @@ function initMarkers(map) {
     let marker = new google.maps.Marker(mark)
   })
 }
-
-const styleObj = [
-  {
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#ebe3cd"
-      }
-    ]
-  },
-  {
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#523735"
-      }
-    ]
-  },
-  {
-    elementType: "labels.text.stroke",
-    stylers: [
-      {
-        color: "#f5f1e6"
-      }
-    ]
-  },
-  {
-    featureType: "administrative",
-    elementType: "geometry.stroke",
-    stylers: [
-      {
-        color: "#c9b2a6"
-      }
-    ]
-  },
-  {
-    featureType: "administrative.land_parcel",
-    elementType: "geometry.stroke",
-    stylers: [
-      {
-        color: "#dcd2be"
-      }
-    ]
-  },
-  {
-    featureType: "administrative.land_parcel",
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#ae9e90"
-      }
-    ]
-  },
-  {
-    featureType: "landscape.natural",
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#dfd2ae"
-      }
-    ]
-  },
-  {
-    featureType: "poi",
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#dfd2ae"
-      }
-    ]
-  },
-  {
-    featureType: "poi",
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#93817c"
-      }
-    ]
-  },
-  {
-    featureType: "poi.park",
-    elementType: "geometry.fill",
-    stylers: [
-      {
-        color: "#a5b076"
-      }
-    ]
-  },
-  {
-    featureType: "poi.park",
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#447530"
-      }
-    ]
-  },
-  {
-    featureType: "road",
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#f5f1e6"
-      }
-    ]
-  },
-  {
-    featureType: "road.arterial",
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#fdfcf8"
-      }
-    ]
-  },
-  {
-    featureType: "road.highway",
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#f8c967"
-      }
-    ]
-  },
-  {
-    featureType: "road.highway",
-    elementType: "geometry.stroke",
-    stylers: [
-      {
-        color: "#e9bc62"
-      }
-    ]
-  },
-  {
-    featureType: "road.highway.controlled_access",
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#e98d58"
-      }
-    ]
-  },
-  {
-    featureType: "road.highway.controlled_access",
-    elementType: "geometry.stroke",
-    stylers: [
-      {
-        color: "#db8555"
-      }
-    ]
-  },
-  {
-    featureType: "road.local",
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#806b63"
-      }
-    ]
-  },
-  {
-    featureType: "transit.line",
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#dfd2ae"
-      }
-    ]
-  },
-  {
-    featureType: "transit.line",
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#8f7d77"
-      }
-    ]
-  },
-  {
-    featureType: "transit.line",
-    elementType: "labels.text.stroke",
-    stylers: [
-      {
-        color: "#ebe3cd"
-      }
-    ]
-  },
-  {
-    featureType: "transit.station",
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#dfd2ae"
-      }
-    ]
-  },
-  {
-    featureType: "water",
-    elementType: "geometry.fill",
-    stylers: [
-      {
-        color: "#b9d3c2"
-      }
-    ]
-  },
-  {
-    featureType: "water",
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#92998d"
-      }
-    ]
-  }
-]
