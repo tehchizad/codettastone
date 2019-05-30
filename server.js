fa87d5de1011458feb45930252d81a2c1c0e6a26
Chad Infanger <tehchizad@gmail.com>
2019-05-29 21:35:23 -0700

    mongoose removed

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-// mongoose = require("mongoose"),
 const bodyParser = require('body-parser')
 const routes = require('./routes/routes')
 const items = require('./routes/api/items')
@@ -11,14 +10,6 @@ app.set('view engine', 'ejs')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-
-// const env = require("node-env-file")
-// env("./config/envVars")
-
-// mongoose
-//   .connect(process.env.MONGO_URI)
-//   .then(() => console.log("Mongo DB connected."))
-//   .catch(err => console.log(err))
 
 app.use('/', routes)
 app.use('/api/items', items)
